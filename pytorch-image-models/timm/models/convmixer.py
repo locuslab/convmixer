8aab7131a7c1ae3690115a5a0ed7eff3c3c20252
ICLR Author <iclr.author@fake-email.net>
2021-10-05 19:02:25 -0400

    add convmixer-768/32

--- pytorch-image-models/timm/models/convmixer.py
+++ pytorch-image-models/timm/models/convmixer.py
@@ -4,6 +4,7 @@ from convmixer import ConvMixer
 from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 from timm.models.registry import register_model
 
+
 _cfg = {
     'url': '',
     'num_classes': 1000, 'input_size': (3, 224, 224), 'pool_size': None,
@@ -11,8 +12,16 @@ _cfg = {
     'mean': IMAGENET_DEFAULT_MEAN, 'std': IMAGENET_DEFAULT_STD, 'classifier': 'head'
 }
 
+
 @register_model
 def convmixer_1536_20(pretrained=False, **kwargs):
     model = ConvMixer(1536, 20, kernel_size=9, patch_size=7, n_classes=1000)
+    model.default_cfg = _cfg
+    return model
+
+
+@register_model
+def convmixer_768_32(pretrained=False, **kwargs):
+    model = ConvMixer(768, 32, kernel_size=7, patch_size=7, n_classes=1000)
     model.default_cfg = _cfg
     return model
